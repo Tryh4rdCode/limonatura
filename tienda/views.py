592b1303922d39335997c3eb1388c868aca95db9
Jonatthan Medalla <jonatthan.medalla@inacapmail.cl>
2024-11-26 03:33:02 -0300

    ok

--- tienda/views.py
+++ tienda/views.py
@@ -2,46 +2,45 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.views.generic import ListView
-from .models import Producto, Categoria_Produ, Categoria_Coleg, Categoria_Sexo
+from .models import Producto, Categoria_Producto, Categoria_Tipo, Categoria_Material
 
 # Aqui creamos las vistas de nuestra tienda
 def tienda(request):
     productos = Producto.objects.all()
-    categorias_prod = Categoria_Produ.objects.all()
-    categorias_cole = Categoria_Coleg.objects.all()
-    categorias_sexo = Categoria_Sexo.objects.all()
+    categorias_prod = Categoria_Producto.objects.all()
+    categorias_tipo = Categoria_Tipo.objects.all()
+    categorias_material = Categoria_Material.objects.all()
 
-# Aqui filtramos los productos por categorias
+    # Aqui filtramos los productos por categorias
     categoria_prod_id = request.GET.get('categoria_prod')
-    categoria_cole_id = request.GET.get('categoria_cole')
-    categoria_sexo_id = request.GET.get('categoria_sexo')
+    categoria_tipo_id = request.GET.get('categoria_tipo')
+    categoria_material_id = request.GET.get('categoria_material')
 
-#Aqui filtramos los productos por categorias
+    # Aqui filtramos los productos por categorias
     if categoria_prod_id:
         productos = productos.filter(categorias_prod_id=categoria_prod_id)
-    if categoria_cole_id:
-        productos = productos.filter(categorias_cole_id=categoria_cole_id)
-    if categoria_sexo_id:
-        productos = productos.filter(categorias_sexo_id=categoria_sexo_id)
+    if categoria_tipo_id:
+        productos = productos.filter(categorias_tipo_id=categoria_tipo_id)
+    if categoria_material_id:
+        productos = productos.filter(categorias_material_id=categoria_material_id)
 
-#Aqui renderizamos la tienda
+    # Aqui renderizamos la tienda
     return render(request, 'tienda/tienda.html', {
         'productos': productos,
         'categorias_prod': categorias_prod,
-        'categorias_cole': categorias_cole,
-        'categorias_sexo': categorias_sexo
+        'categorias_tipo': categorias_tipo,
+        'categorias_material': categorias_material
     })
 
-#Aqui renderizamos el carrito
+# Aqui renderizamos el carrito
 def confirmar_pedido(request):
     # Lógica para confirmar el pedido
     return render(request, 'tienda/confirmar_pedido.html')
 
-#Aqui renderizamos el detalle del producto
+# Aqui renderizamos el detalle del producto
 def detalle_producto(request, producto_id):
     producto = get_object_or_404(Producto, id=producto_id)
     return render(request, 'tienda/detalle_producto.html', {'producto': producto})
-
 
 def comprar_producto(request, producto_id):
     producto = get_object_or_404(Producto, id=producto_id)
@@ -53,32 +52,31 @@ def comprar_producto(request, producto_id):
         messages.error(request, 'Lo sentimos, este producto está agotado.')
     return redirect('nstienda:detalle_producto', producto_id=producto.id)
 
-#Aqui creamos la vista de la tienda con ListView
+# Aqui creamos la vista de la tienda con ListView
 class ProductoListView(ListView):
     model = Producto
     template_name = 'tienda/tienda.html'
     context_object_name = 'productos'
 
-
     def get_queryset(self):
         queryset = super().get_queryset()
         categoria_prod_id = self.request.GET.get('categoria_prod')
-        categoria_cole_id = self.request.GET.get('categoria_cole')
-        categoria_sexo_id = self.request.GET.get('categoria_sexo')
+        categoria_tipo_id = self.request.GET.get('categoria_tipo')
+        categoria_material_id = self.request.GET.get('categoria_material')
         
         if categoria_prod_id:
             queryset = queryset.filter(categorias_prod_id=categoria_prod_id)
-        if categoria_cole_id:
-            queryset = queryset.filter(categorias_cole_id=categoria_cole_id)
-        if categoria_sexo_id:
-            queryset = queryset.filter(categorias_sexo_id=categoria_sexo_id)
+        if categoria_tipo_id:
+            queryset = queryset.filter(categorias_tipo_id=categoria_tipo_id)
+        if categoria_material_id:
+            queryset = queryset.filter(categorias_material_id=categoria_material_id)
         
         return queryset
 
-#Aqui creamos el metodo get_context_data
+    # Aqui creamos el metodo get_context_data
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['categorias_prod'] = Categoria_Produ.objects.all()
-        context['categorias_cole'] = Categoria_Coleg.objects.all()
-        context['categorias_sexo'] = Categoria_Sexo.objects.all()
+        context['categorias_prod'] = Categoria_Producto.objects.all()
+        context['categorias_tipo'] = Categoria_Tipo.objects.all()
+        context['categorias_material'] = Categoria_Material.objects.all()
         return context
